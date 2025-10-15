d0c02190473644dd8daf54f2b3729236692a6a4b
Vladyslav Pihol <freepgvlad@gmail.com>
2025-10-15 16:41:01 +0300

    Solution3

--- cinema/views.py
+++ cinema/views.py
@@ -9,7 +9,11 @@ from rest_framework.authentication import (
 )
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+    AllowAny
+)
 from rest_framework.response import Response
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework.viewsets import GenericViewSet
